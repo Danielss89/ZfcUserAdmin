506df75a2b0ca4af3d5a679176eb5c5a3cacaba5
Vladimir Garvardt <vgarvardt@gmail.com>
2013-03-18 16:57:21 +0400

    Allow mapper class extended from ZfcUserAdmin\Mapper\UserZendDb usage (plus little code clean up)

--- Module.php
+++ Module.php
@@ -10,6 +10,11 @@
 namespace ZfcUserAdmin;
 
 use Zend\Mvc\ModuleRouteListener;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use ZfcUser\Form\RegisterFilter;
+use ZfcUser\Mapper\UserHydrator;
+use ZfcUser\Validator\NoRecordExists;
+
 
 class Module
 {
@@ -34,56 +39,61 @@ class Module
         return array(
             'invokables' => array(
                 'ZfcUserAdmin\Form\EditUser' => 'ZfcUserAdmin\Form\EditUser',
-                'zfcuseradmin_user_service'       => 'ZfcUserAdmin\Service\User',
+                'zfcuseradmin_user_service' => 'ZfcUserAdmin\Service\User',
             ),
             'factories' => array(
-                'zfcuseradmin_module_options' => function ($sm) {
+                'zfcuseradmin_module_options' => function (ServiceLocatorInterface $sm) {
                     $config = $sm->get('Config');
                     return new Options\ModuleOptions(isset($config['zfcuseradmin']) ? $config['zfcuseradmin'] : array());
                 },
-                'zfcuseradmin_edituser_form' => function($sm) {
+                'zfcuseradmin_edituser_form' => function (ServiceLocatorInterface $sm) {
+                    /** @var $options \ZfcUserAdmin\Options\ModuleOptions */
                     $options = $sm->get('zfcuseradmin_module_options');
                     $form = new Form\EditUser(null, $options, $sm);
                     return $form;
                 },
-                'zfcuseradmin_createuser_form' => function($sm) {
+                'zfcuseradmin_createuser_form' => function (ServiceLocatorInterface $sm) {
+                    /** @var $zfcUserOptions \ZfcUser\Options\UserServiceOptionsInterface */
                     $zfcUserOptions = $sm->get('zfcuser_module_options');
+                    /** @var $zfcUserAdminOptions \ZfcUserAdmin\Options\ModuleOptions */
                     $zfcUserAdminOptions = $sm->get('zfcuseradmin_module_options');
                     $form = new Form\CreateUser(null, $zfcUserAdminOptions, $zfcUserOptions, $sm);
-                    $filter = new \ZfcUser\Form\RegisterFilter(
-                        new \ZfcUser\Validator\NoRecordExists(array(
+                    $filter = new RegisterFilter(
+                        new NoRecordExists(array(
                             'mapper' => $sm->get('zfcuser_user_mapper'),
-                            'key'    => 'email'
+                            'key' => 'email'
                         )),
-                        new \ZfcUser\Validator\NoRecordExists(array(
+                        new NoRecordExists(array(
                             'mapper' => $sm->get('zfcuser_user_mapper'),
-                            'key'    => 'username'
+                            'key' => 'username'
                         )),
                         $zfcUserOptions
                     );
-                    if($zfcUserAdminOptions->getCreateUserAutoPassword())
-                    {
+                    if ($zfcUserAdminOptions->getCreateUserAutoPassword()) {
                         $filter->remove('password')->remove('passwordVerify');
                     }
                     $form->setInputFilter($filter);
                     return $form;
                 },
-                'zfcuser_user_mapper' => function ($sm) {
+                'zfcuser_user_mapper' => function (ServiceLocatorInterface $sm) {
+                    /** @var $config \ZfcUserAdmin\Options\ModuleOptions */
                     $config = $sm->get('zfcuseradmin_module_options');
                     $mapperClass = $config->getUserMapper();
-                    if ($mapperClass == 'ZfcUserAdmin\Mapper\UserZendDb') {
-                        $zfcUserOptions = $sm->get('zfcuser_module_options');
-
-                        $mapper = new $mapperClass;
-                        $mapper->setDbAdapter($sm->get('zfcuser_zend_db_adapter'));
-                        $entityClass = $zfcUserOptions->getUserEntityClass();
-                        $mapper->setEntityPrototype(new $entityClass);
-                        $mapper->setHydrator(new \ZfcUser\Mapper\UserHydrator());
-                    } else {
+                    if (stripos($mapperClass, 'doctrine') !== false) {
                         $mapper = new $mapperClass(
                             $sm->get('zfcuser_doctrine_em'),
                             $sm->get('zfcuser_module_options')
                         );
+                    } else {
+                        /** @var $zfcUserOptions \ZfcUser\Options\UserServiceOptionsInterface */
+                        $zfcUserOptions = $sm->get('zfcuser_module_options');
+
+                        /** @var $mapper \ZfcUserAdmin\Mapper\UserZendDb */
+                        $mapper = new $mapperClass();
+                        $mapper->setDbAdapter($sm->get('zfcuser_zend_db_adapter'));
+                        $entityClass = $zfcUserOptions->getUserEntityClass();
+                        $mapper->setEntityPrototype(new $entityClass);
+                        $mapper->setHydrator(new UserHydrator());
                     }
 
                     return $mapper;
