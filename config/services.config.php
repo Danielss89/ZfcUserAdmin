a7f929819a529d26d1589f0def503a7dfc47179e
Stijn Haulotte <stijnhau@gmail.com>
2014-06-14 15:41:50 +0200

    Fix error with laster ZfcUser

--- config/services.config.php
+++ config/services.config.php
@@ -11,6 +11,7 @@ use ZfcUser\Validator\NoRecordExists;
 use ZfcUserAdmin\Form;
 use ZfcUserAdmin\Options;
 use ZfcUserAdmin\Validator\NoRecordExistsEdit;
+use Zend\Crypt\Password\Bcrypt;
 
 return array(
     'invokables' => array(
@@ -89,7 +90,7 @@ return array(
                 $mapper->setDbAdapter($sm->get('zfcuser_zend_db_adapter'));
                 $entityClass = $zfcUserOptions->getUserEntityClass();
                 $mapper->setEntityPrototype(new $entityClass);
-                $mapper->setHydrator(new UserHydrator());
+                $mapper->setHydrator(new UserHydrator(new Bcrypt));
             }
 
             return $mapper;
