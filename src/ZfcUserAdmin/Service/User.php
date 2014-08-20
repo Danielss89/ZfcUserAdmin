de31162edb59c2741780cfa87c31118c61c68553
Stepan Stepanov <stepan.stepanov@experian.com>
2014-08-20 16:07:27 +0200

    Add option auto_password_length

--- src/ZfcUserAdmin/Service/User.php
+++ src/ZfcUserAdmin/Service/User.php
@@ -50,7 +50,7 @@ class User extends EventProvider implements ServiceManagerAwareInterface
 
         $argv = array();
         if ($this->getOptions()->getCreateUserAutoPassword()) {
-            $argv['password'] = Rand::getString(8);
+            $argv['password'] = $this->generatePassword();
         } else {
             $argv['password'] = $user->getPassword();
         }
@@ -89,7 +89,7 @@ class User extends EventProvider implements ServiceManagerAwareInterface
         // then check if admin wants to change user password
         if ($this->getOptions()->getAllowPasswordChange()) {
             if (!empty($data['reset_password'])) {
-                $argv['password'] = Rand::getString(8);
+                $argv['password'] = $this->generatePassword();
             } elseif (!empty($data['password'])) {
                 $argv['password'] = $data['password'];
             }
@@ -111,6 +111,14 @@ class User extends EventProvider implements ServiceManagerAwareInterface
         $this->getUserMapper()->update($user);
         $this->getEventManager()->trigger(__FUNCTION__ . '.post', $this, $argv);
         return $user;
+    }
+
+    /**
+     * @return string
+     */
+    public function generatePassword()
+    {
+        return Rand::getString($this->getOptions()->getAutoPasswordLength());
     }
 
     protected function getAccessorName($property, $set = true)
